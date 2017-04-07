00ab9fb5c6039320a05689ce45858337a9b37526
Paul Frederiks <paul.frederiks@uu.nl>
2017-04-07 16:11:28 +0200

    Add max revisions and blacklist to configuration

--- uuIlabConstants.r
+++ uuIlabConstants.r
@@ -7,3 +7,10 @@ UUPRIMARYRESOURCES = list("irodsResc");
 
 # \constant UUREPLICATIONRESOURCE
 UUREPLICATIONRESOURCE = "irodsRescRepl";
+
+# \constant UUMAXREVISIONSIZE
+UUMAXREVISIONSIZE = 2000000000; # 2GB as in 2 * 1000 * 1000 * 1000
+
+# \constant UUBLACKLIST
+UUBLACKLIST = list("._*", ".DS_Store"); 
+
