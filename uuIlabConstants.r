4230e979695b722cd7c704e4aa74a0f139736d1e
Paul Frederiks <p.frederiks@uu.nl>
2017-09-15 15:50:29 +0200

    Introduce dataCite API configuration

--- uuIlabConstants.r
+++ uuIlabConstants.r
@@ -14,3 +14,17 @@ UUMAXREVISIONSIZE = double("2000000000"); # 2GB as in 2 * 1000 * 1000 * 1000
 # \constant UUBLACKLIST
 UUBLACKLIST = list("._*", ".DS_Store"); 
 
+# \constant UUINSTANCENAME
+UUINSTANCENAME = "i-lab";
+
+# \constant UUDOIPREFIX
+UUDOIPREFIX = "10.5072"; # Test prefix
+
+# \constant UUDOIYODAPREFIX
+UUDOIYODAPREFIX = "UU01";
+
+# \constant UUDATACITESERVER
+UUDATACITESERVER = "https://mds.test.datacite.org";
+
+# \constant UURANDOMIDLENGTH
+UURANDOMIDLENGTH = 6;
