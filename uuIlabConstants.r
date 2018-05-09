28b296edcc08547495c059cd6f76a4813163ba2d
Lazlo Westerhof <l.r.westerhof@uu.nl>
2018-05-09 15:16:44 +0200

    Remove deprecated constants.

--- uuIlabConstants.r
+++ uuIlabConstants.r
@@ -1,5 +1,5 @@
 # \file
-# \copyright Copyright (c) 2016, Utrecht university. All rights reserved
+# \copyright Copyright (c) 2016-2018, Utrecht university. All rights reserved
 # \license GPLv3, see LICENSE
 
 # \constant UUPRIMARYRESOURCES
@@ -13,18 +13,3 @@ UUMAXREVISIONSIZE = double("2000000000"); # 2GB as in 2 * 1000 * 1000 * 1000
 
 # \constant UUBLACKLIST
 UUBLACKLIST = list("._*", ".DS_Store"); 
-
-# \constant UUINSTANCENAME
-UUINSTANCENAME = "i-lab";
-
-# \constant UUDOIPREFIX
-UUDOIPREFIX = "10.5072"; # Test prefix
-
-# \constant UUDOIYODAPREFIX
-UUDOIYODAPREFIX = "UU01";
-
-# \constant UUDATACITESERVER
-UUDATACITESERVER = "https://mds.test.datacite.org";
-
-# \constant UURANDOMIDLENGTH
-UURANDOMIDLENGTH = 6;
